8321f344698281147443aed140e4b4f0369a4edc
AarenNepal <nepalaaren63@gmail.com>
2026-01-19 15:23:19 +0545

    (ADD): Added the models to django ORM

--- guff/admin.py
+++ guff/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import UserProfile, SubscriptionPlan, UserSubscription, DiscordIntegration, WhatsAppIntegration, Payment
 # Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(SubscriptionPlan)
+admin.site.register(UserSubscription)
+admin.site.register(DiscordIntegration)
+admin.site.register(WhatsAppIntegration)
+admin.site.register(Payment)
