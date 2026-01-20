0fdf247e64cf919d2108cf3b89e42feb54435d26
AarenNepal <nepalaaren63@gmail.com>
2026-01-20 15:07:24 +0545

    ADD: First Initial Frontend Visualisation

--- guff/urls.py
+++ guff/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('main', views.main, name="main"),
+    path('signup', views.signup, name="signup"),
+    path('login', views.login, name="login"),
+    path('contacts', views.contacts, name="contacts"),
+    path('dashboard', views.dashboard, name="dashboard"),
+    path('', views.landing, name="landing"),
+    path('profile', views.creator_profile, name="creator_profile"),
+    path('subscription', views.subscription, name="subscription"),
 ]
--- guff/views.py
+++ guff/views.py
@@ -3,3 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 def main(request):
     return render(request, 'guff/main.html')
+
+def signup(request):
+    return render(request, 'guff/signup.html')
+
+def login(request):
+    return render(request, 'guff/login.html')
+
+def contacts(request):
+    return render(request, 'guff/contacts.html')
+
+def dashboard(request):
+    return render(request, 'guff/dashboard.html')
+
+def landing(request):
+    return render(request, 'guff/landing.html')
+
+def creator_profile(request):
+    return render(request, 'guff/creator_profile.html')
+
+def subscription(request):
+    return render(request, 'guff/subscription.html')
