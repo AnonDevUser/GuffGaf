cbb2b9054399c1b84c2b3e5659fb71e1f105f50b
AarenNepal <nepalaaren63@gmail.com>
2026-01-26 22:20:56 +0545

    ADD: serializers for models

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,18 @@
 from rest_framework import serializers
 from guff.models import UserProfile, DiscordIntegration, WhatsAppIntegration
 
-... 
+class ProfileSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source="user.username",  read_only=True)
+    class Meta:
+        model = UserProfile
+        fields = ['username']
+
+class DiscordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DiscordIntegration
+        fields = ['guild_id', 'role_id']
+
+class WhatsAppSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WhatsAppIntegration 
+        fields = ['group_link']
--- api/subscription_serializers.py
+++ api/subscription_serializers.py
@@ -1,3 +1,21 @@
 from rest_framework import serializers
 from guff.models import UserSubscription, SubscriptionPlan, Payment
-...
+
+class UserSubSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='buyer.user.username', read_only=True)
+    plan_name = serializers.CharField(source='plan.name', read_only=True)
+    price = serializers.DecimalField(source='plan.price', max_digits=10, decimal_places=2, read_only=True)
+    interval = serializers.CharField(source='plan.interval', read_only=True)
+    class Meta:
+        model = UserSubscription
+        fields = ['username', 'plan_name', 'price', 'interval']
+
+class PlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubscriptionPlan
+        fields = ['id', 'creator', 'subscription_bio', 'name', 'price', 'interval']
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment 
+        fields = ['id', 'buyer', 'plan', 'amount', 'gateway', 'transaction_id', 'status', 'created_at']
