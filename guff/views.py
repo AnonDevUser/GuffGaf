a9201d4db1d41aaaee1c4525369a198362eb7804
AarenNepal <nepalaaren63@gmail.com>
2026-01-26 13:30:11 +0545

    TBD: View setup, models imported

--- guff/views.py
+++ guff/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth import login, logout 
+from .models import UserProfile 
+from django.contrib.auth.models import User
 
 # Create your views here.
 def main(request):
     return render(request, 'guff/main.html')
+
+def user_login(request):
+    """
+    logs in the user using (Django User)
+    
+    :parameters request: django request
+    """
+    ...
+
+def user_logout(request):
+    ...
+
+def signup(request):
+    ...
+
+def user_profile(request):
+    ...
+
+def subscribe(request):
+    ...
+
