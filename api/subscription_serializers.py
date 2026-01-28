09a01b17c6990ef76cb67186be041afa8e6ab3b5
AarenNepal <nepalaaren63@gmail.com>
2026-01-28 18:32:25 +0545

    fix(UserSubSerializer): added validation to avoid plan duplication for user and allow POST method

--- api/subscription_serializers.py
+++ api/subscription_serializers.py
@@ -6,9 +6,30 @@ class UserSubSerializer(serializers.ModelSerializer):
     plan_name = serializers.CharField(source='plan.name', read_only=True)
     price = serializers.DecimalField(source='plan.price', max_digits=10, decimal_places=2, read_only=True)
     interval = serializers.CharField(source='plan.interval', read_only=True)
+
     class Meta:
         model = UserSubscription
-        fields = ['username', 'plan_name', 'price', 'interval']
+        fields = [
+            'id',
+            'plan',
+            'username',
+            'plan_name',
+            'price',
+            'interval',
+        ]
+        read_only_fields = ['username', 'plan_name', 'price', 'interval']
+
+    def validate(self, attrs):
+        buyer = self.context['buyer']
+        plan = attrs['plan']
+
+        if UserSubscription.objects.filter(buyer=buyer, plan=plan).exists():
+            raise serializers.ValidationError(
+                {"plan": "You are already subscribed to this plan."}
+            )
+
+        return attrs
+
 
 class PlanSerializer(serializers.ModelSerializer):
     creator = serializers.CharField(source="creator.user.username", read_only=True)
