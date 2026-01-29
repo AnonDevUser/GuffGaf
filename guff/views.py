45f1d97470d0789bf4410cd16d71e64f18c19b14
AarenNepal <nepalaaren63@gmail.com>
2026-01-29 10:46:01 +0545

    fix(auth): added code ignored by merger

--- guff/views.py
+++ guff/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.contrib.auth import login, logout 
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.contrib.auth import login, logout, authenticate
 from .models import UserProfile 
 from django.contrib.auth.models import User
 
@@ -9,15 +10,31 @@ def main(request):
 
 def login(request):
     if request.method == "POST":
-        ...
-    else:
-        return render(request, "guff/login.html")
+        username = request.POST.get('username', '').strip
+        password = request.POST.get('password', '').strip
+        if not username or password:
+            return JsonResponse({
+                'status':'error',
+                'message':'provide both username and password correctly'
+            }, status=400)
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard', {
+                'user':request.user
+            })
+
+    return render(request, "guff/login.html")
 
 def logout(request):
     if request.method == "POST":
-        ...
-    else:
-        return render(request, "guff/main.html")
+        if request.user.is_authenticated:
+            logout(request)
+            return JsonResponse({'status':'success'}, status=200)
+        else:
+            return redirect('login')
+            ...
+    return render(request, "guff/main.html")
 
 def signup(request):
     if request.method == "POST":
@@ -32,4 +49,3 @@ def user_profile(request, id):
 
 def subscribe(request):
     ...
-
