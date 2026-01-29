44cdee62eea1f5c810d4b99e2f5e89c11b76ba09
AarenNepal <nepalaaren63@gmail.com>
2026-01-29 13:53:49 +0545

    fix+feat(auth flow): write authentication flow and fix bugs

--- guff/urls.py
+++ guff/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('main', views.main, name="main"),
     path('signup', views.signup, name="signup"),
-    path('login', views.login, name="login"),
+    path('login', views.login_view, name="login"),
+    path('logout', views.logout_view, name="logout"),
     path('contacts', views.contacts, name="contacts"),
     path('dashboard', views.dashboard, name="dashboard"),
     path('', views.landing, name="landing"),
--- guff/views.py
+++ guff/views.py
@@ -2,39 +2,49 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib.auth import login, logout, authenticate
 from .models import UserProfile 
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 # Create your views here.
 def main(request):
     return render(request, 'guff/main.html')
 
-def login(request):
-    if request.method == "POST":
-        username = request.POST.get('username', '').strip
-        password = request.POST.get('password', '').strip
-        if not username or password:
-            return JsonResponse({
-                'status':'error',
-                'message':'provide both username and password correctly'
-            }, status=400)
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('dashboard', {
-                'user':request.user
-            })
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
 
+    if request.method == "POST":
+        username = request.POST.get('username', '').strip()
+        password = request.POST.get('password', '').strip()
+
+        if not username or not password:
+            return JsonResponse({
+                'status': 'error',
+                'message': 'provide both username and password correctly'
+            }, status=400)
+        
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            return JsonResponse({
+                'status': 'error',
+                'message': 'invalid credentials'
+            }, status=401)
+        login(request, user)
+        profile = get_object_or_404(UserProfile, user=user)
+        if profile.is_creator:
+            return redirect('dashboard')
+        else:
+            return redirect('landing') #change to subscription after logic developed
     return render(request, "guff/login.html")
 
-def logout(request):
+@login_required(login_url='login')
+def logout_view(request):
     if request.method == "POST":
         if request.user.is_authenticated:
             logout(request)
-            return JsonResponse({'status':'success'}, status=200)
-        else:
-            return redirect('login')
-            ...
-    return render(request, "guff/main.html")
+            return redirect('landing')
+    return redirect('login')
 
 def signup(request):
     if request.method == "POST":
@@ -44,8 +54,38 @@ def signup(request):
 
 def user_profile(request, id):
     return render(request, "guff/profile.html", {
-        id: id
+        "id": id
     })
 
 def subscribe(request):
     ...
+def contacts(request):
+    return render(request, "guff/contacts.html")
+
+@login_required(login_url='login')
+def dashboard(request):
+    creator = get_object_or_404(UserProfile, user=request.user)
+    if creator.is_creator:
+        return render(request, 'guff/dashboard.html')
+    else:
+        return redirect('landing')
+
+def landing(request):
+    return render(request, "guff/landing.html")
+
+@login_required(login_url='login')
+#buggy - need ID passed (later)
+def creator_profile(request):
+    return render(request, "guff/creator_profile.html", {
+        "user":request.user 
+    })
+
+@login_required(login_url='login')
+def subscription(request):
+    creator = get_object_or_404(UserProfile, user=request.user)
+    if not creator.is_creator:
+        return render(request, "guff/subscription.html", {
+            "user":request.user 
+        })
+    else:
+        return redirect('dashboard')
